21540eb0c4623d5095ca50eef92a3b39c84d3591
yaz_uaz <zayarnyy74@gmail.com>
2019-09-29 09:56:09 +0500

    📦 NEW: Add moving logic to main pin element

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 (function () {
   var PIN_Y_POINTER = 16;
+  var PIN_Y_MIN = 130;
+  var PIN_Y_MAX = 630;
 
   var isPageActive = false;
 
@@ -37,7 +39,7 @@
     isPageActive = true;
   };
 
-  var getAddressCoords = function () {
+  var updateAddressCoords = function () {
     var xPointer = window.map.pinMainElem.offsetWidth / 2;
     var yPointer = isPageActive ? window.map.pinMainElem.offsetHeight + PIN_Y_POINTER
       : window.map.pinMainElem.offsetHeight / 2;
@@ -46,21 +48,99 @@
     window.adForm.inputAddressElem.value = xCoord + ', ' + yCoord;
   };
 
-  window.map.pinMainElem.addEventListener('mousedown', function () {
-    activatePage();
-    getAddressCoords();
+  var getXCoord = function (x) {
+    var start = window.map.pinContainerElem.offsetLeft;
+    var end = window.map.pinContainerElem.offsetLeft + window.map.pinContainerElem.offsetWidth - window.map.pinMainElem.offsetWidth;
+
+    if (x < start) {
+      return start;
+    }
+
+    if (x > end) {
+      return end;
+    }
+    return x;
+  };
+
+  var getYCoord = function (y) {
+    var start = window.map.pinContainerElem.offsetTop + PIN_Y_MIN - window.map.pinMainElem.offsetHeight - PIN_Y_POINTER;
+    var end = window.map.pinContainerElem.offsetTop + PIN_Y_MAX - window.map.pinMainElem.offsetHeight - PIN_Y_POINTER;
+
+    if (y < start) {
+      return start;
+    }
+
+    if (y > end) {
+      return end;
+    }
+    return y;
+  };
+
+  window.map.pinMainElem.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    if (!isPageActive) {
+      activatePage();
+      updateAddressCoords();
+    }
+
+    var startCoords = {
+      x: window.map.pinMainElem.offsetLeft,
+      y: window.map.pinMainElem.offsetTop
+    };
+
+    var startMouseCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startMouseCoords.x - moveEvt.clientX,
+        y: startMouseCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: startCoords.x - shift.x,
+        y: startCoords.y - shift.y
+      };
+
+      startMouseCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY,
+      };
+
+      window.map.pinMainElem.style.left = getXCoord(startCoords.x) + 'px';
+      window.map.pinMainElem.style.top = getYCoord(startCoords.y) + 'px';
+
+      updateAddressCoords();
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      updateAddressCoords();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 
   window.map.pinMainElem.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === window.utils.KEYCODES.ENTER) {
+    if (evt.keyCode === window.utils.KEYCODES.ENTER && !isPageActive) {
       activatePage();
-      getAddressCoords();
+      updateAddressCoords();
     }
   });
 
   var initPage = function () {
     deactivatePage();
-    getAddressCoords();
+    updateAddressCoords();
   };
 
   initPage();
