e6f798ad61d2180258a85eddeda7bea8b0ae3575
yaz_uaz <zayarnyy74@gmail.com>
2019-10-11 15:30:36 +0500

    📦 NEW: Add filter logic to all filter form elements

--- js/map.js
+++ js/map.js
@@ -5,6 +5,19 @@
     X: 570,
     Y: 375
   };
+  var PRICE = {
+    LOW: {
+      MIN: 0,
+      MAX: 9999
+    },
+    MIDDLE: {
+      MIN: 10000,
+      MAX: 49999
+    },
+    HIGH: {
+      MIN: 50000
+    }
+  };
 
   var mapElem = document.querySelector('.map');
   var filterContainerElem = mapElem.querySelector('.map__filters-container');
@@ -12,6 +25,15 @@
   var pinMainElem = pinContainerElem.querySelector('.map__pin--main');
   var filterFormElem = mapElem.querySelector('.map__filters');
   var filterTypeSelect = filterFormElem.querySelector('#housing-type');
+  var filterPriceSelect = filterFormElem.querySelector('#housing-price');
+  var filterRoomsSelect = filterFormElem.querySelector('#housing-rooms');
+  var filterGuestsSelect = filterFormElem.querySelector('#housing-guests');
+  var filterWifiInput = filterFormElem.querySelector('#filter-wifi');
+  var filterDishwasherInput = filterFormElem.querySelector('#filter-dishwasher');
+  var filterParkingInput = filterFormElem.querySelector('#filter-parking');
+  var filterWasherInput = filterFormElem.querySelector('#filter-washer');
+  var filterElevatorInput = filterFormElem.querySelector('#filter-elevator');
+  var filterConditionerInput = filterFormElem.querySelector('#filter-conditioner');
 
   var destroyCard = function () {
     var card = mapElem.querySelector('.map__card.popup');
@@ -73,9 +95,80 @@
     });
   };
 
-  filterTypeSelect.addEventListener('input', function () {
+  var filterByPrice = function (data) {
+    switch (filterPriceSelect.value) {
+      case 'any':
+        return data;
+      case 'low':
+        return data.filter(function (elem) {
+          return elem.offer.price > +PRICE.LOW.MIN && elem.offer.price <= +PRICE.LOW.MAX;
+        });
+      case 'middle':
+        return data.filter(function (elem) {
+          return elem.offer.price > +PRICE.MIDDLE.MIN && elem.offer.price <= +PRICE.MIDDLE.MAX;
+        });
+      case 'high':
+        return data.filter(function (elem) {
+          return elem.offer.price > +PRICE.MIDDLE.MIN;
+        });
+    }
+    return data;
+  };
+
+  var filterByRooms = function (data) {
+    if (filterRoomsSelect.value === 'any') {
+      return data;
+    }
+
+    return data.filter(function (elem) {
+      return elem.offer.rooms === +filterRoomsSelect.value;
+    });
+  };
+
+  var filterByGuests = function (data) {
+    if (filterGuestsSelect.value === 'any') {
+      return data;
+    }
+
+    return data.filter(function (elem) {
+      return elem.offer.guests === +filterGuestsSelect.value;
+    });
+  };
+
+  var filterByFeatures = function (data, checkbox) {
+    if (!checkbox.checked) {
+      return data;
+    }
+
+    return data.filter(function (elem) {
+      return elem.offer.features.indexOf(checkbox.value) !== -1;
+    });
+  };
+
+  var filterPinData = function (data) {
+    var result = data;
+    result = filterByType(result);
+    result = filterByPrice(result);
+    result = filterByRooms(result);
+    result = filterByGuests(result);
+    result = filterByFeatures(result, filterWifiInput);
+    result = filterByFeatures(result, filterDishwasherInput);
+    result = filterByFeatures(result, filterParkingInput);
+    result = filterByFeatures(result, filterWasherInput);
+    result = filterByFeatures(result, filterElevatorInput);
+    result = filterByFeatures(result, filterConditionerInput);
+    return result;
+  };
+
+  var refreshPins = window.utils.debounce(function () {
     destroyPins();
-    pinContainerElem.appendChild(window.renderPins(filterByType(window.app.pinData)));
+    pinContainerElem.appendChild(window.renderPins(filterPinData(window.app.pinData)));
+  });
+
+  filterContainerElem.addEventListener('input', function (evt) {
+    if (evt.target.matches('select, input')) {
+      refreshPins();
+    }
   });
 
   var activateMap = function () {
